5a93d528606bddf068c36d16a115b262cca0608a
Geoff Blair <geoff@mousepox.io>
2021-12-02 04:57:38 -0700

    Day 1, Part 2

--- lib/day1.lua
+++ lib/day1.lua
@@ -6,6 +6,16 @@ local depth_increases = 0
 
 local prev_depth = nil
 
+local window_size = 3
+
+local window = {}
+
+local window_sum = 0
+
+local prev_window_sum = nil
+
+local window_depth_increases = 0
+
 for line in input do
 	local depth = tonumber(line)
 
@@ -14,6 +24,24 @@ for line in input do
 	end
 
 	prev_depth = depth
+
+	table.insert(window, depth)
+
+	window_sum = window_sum + depth
+
+	if #window > window_size then
+		window_sum = window_sum - window[1]
+		table.remove(window, 1)
+	end
+
+	if prev_window_sum ~= nil and window_sum > prev_window_sum then
+		window_depth_increases = window_depth_increases + 1
+	end
+
+	if #window == window_size then
+		prev_window_sum = window_sum
+	end
 end
 
-print("Day 1: " .. depth_increases)
+print("Day 1, Part 1: " .. depth_increases)
+print("Day 1, Part 2: " .. window_depth_increases)
